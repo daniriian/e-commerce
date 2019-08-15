8d06c1a6de9dcbb92d1b34c161b663c4e16b4e85
Daniel RT <daniriian@users.noreply.github.com>
2019-08-15 13:25:20 +0300

    create header commponent and add to all pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from './components/header/header';
 import HomePage from './pages/homepage/hompage';
 import ShopPage from './pages/shop/shop';
 
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
