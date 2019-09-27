4afa183917dfd6ac7b250091df872a11efec6206
DRT <dryan251@gmail.com>
2019-09-27 14:47:43 +0300

    rezolve a problem with button position inside collection-item

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+
+import CustomButton from '../custom-button/custom-button';
+
 import './collection-item.scss';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
   return (
-    <div className='collection-item'>
-      <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
-      <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
       </div>
+      <CustomButton inverted>Add to cart</CustomButton>
     </div>
   );
 };
