0d4e97620cdb36c578b5a7cb3d28cf5df3be1c4c
DRT <dryan251@gmail.com>
2019-09-30 10:44:15 +0300

    updating shop data with new data

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import CustomButton from '../custom-button/custom-button';
 
-import './cart-dropdown.scss'; 
+import './cart-dropdown.scss';
 
 const CartDropdown = () => {
-    return (
-        <div className="cart-dropdown">
-            <div className="cart-items" />
-            <CustomButton>Go To Checkout</CustomButton>
-        </div>
-    );
-}
- 
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items" />
+      <CustomButton>Go To Checkout</CustomButton>
+    </div>
+  );
+};
+
 export default CartDropdown;
--- src/components/collectionPreview/collectionPreview.jsx
+++ src/components/collectionPreview/collectionPreview.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 import './collectionPreview.scss';
@@ -6,13 +7,13 @@ import CollectionItem from '../collection-item/collection-item';
 
 const CollectionPreview = ({ title, items }) => {
   return (
-    <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <div className='preview'>
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
         {items
           .filter((item, index) => index < 4)
-          .map(({ id, ...restOfProps }) => (
-            <CollectionItem key={id} {...restOfProps} />
+          .map(item => (
+            <CollectionItem key={item.id} item={item} />
           ))}
       </div>
     </div>
