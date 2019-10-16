716c0c79fca63447fc84b00c9067ed86be6a2d3d
DRT <dryan251@gmail.com>
2019-10-16 14:55:37 +0300

    move directory state to redux store and use reselect for directory component

--- src/components/checkout-item/checkout-item.jsx
+++ src/components/checkout-item/checkout-item.jsx
@@ -1,3 +1,7 @@
+/* eslint-disable no-shadow */
+/* eslint-disable jsx-a11y/interactive-supports-focus */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import './checkout-item.scss';
--- src/components/directory/directory.jsx
+++ src/components/directory/directory.jsx
@@ -1,68 +1,27 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
 import React from 'react';
 import './directory.scss';
 
-import MenuItem from '../../components/menu-item/menu-item';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-class Directory extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id: 1,
-          linkUrl: 'shop/hats'
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id: 2,
-          linkUrl: 'shop/jackets'
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id: 3,
-          linkUrl: 'shop/sneakers'
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens'
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens'
-        }
-      ]
-    };
-  }
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
-  render() {
-    const { sections } = this.state;
-    return (
-      <div className='directory-menu'>
-        {sections.map(s => {
-          return (
-            <MenuItem
-              key={s.id}
-              title={s.title.toUpperCase()}
-              imageUrl={s.imageUrl}
-              size={s.size}
-              history={this.props.history}
-              linkUrl={s.linkUrl}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+import MenuItem from '../menu-item/menu-item';
 
-export default Directory;
+const Directory = ({ sections }) => {
+  return (
+    <div className="directory-menu">
+      {sections.map(({ id, ...otherSectionProps }) => {
+        return <MenuItem key={id} {...otherSectionProps} />;
+      })}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
+
+export default connect(mapStateToProps)(Directory);
