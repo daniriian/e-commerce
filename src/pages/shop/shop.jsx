26e841580fcffdc2cecde337f982aa2876fa9ad1
DRT <dryan251@gmail.com>
2019-10-29 09:27:35 +0200

    making shop page tio use get() to retrieve data from firebase

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -29,10 +29,9 @@ class ShopPage extends Component {
 
   componentDidMount() {
     const { updateCollections } = this.props;
-
     const collectionRef = firestore.collection('collections');
 
-    this.unsubcribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
       this.setState({ loading: false });
