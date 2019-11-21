a18d46f9a926b1fc7ff1a3253bdcd20b498f6a2b
DRT <dryan251@gmail.com>
2019-11-21 13:57:13 +0200

    separate our app into client folder, added express server to help handle backend stripe paymentp

---
+++ client/src/components/menu-item/menu-item.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './menu-item.scss';
+import { withRouter } from 'react-router-dom';
+
+const MenuItem = props => {
+  const { title, imageUrl, size, history, linkUrl, match } = props;
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(MenuItem);
