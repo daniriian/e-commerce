3b25b00243647b6be77530ba8f8d6cae1ae141f3
DRT <dryan251@gmail.com>
2019-11-26 13:30:29 +0200

    update collections component to use context api with Provider

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -1,28 +1,31 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { connect } from 'react-redux';
-import { selectCollection } from '../../redux/shop/shop.selectors';
 
 import CollectionItem from '../../components/collection-item/collection-item';
 
+import CollectionsContext from '../../contexts/collections/collections.context';
+
 import './collection.scss';
 
-const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+const CollectionPage = ({ match }) => {
   return (
-    <div className="collection-page">
-      <h1 className="title">{title}</h1>
-      <div className="items">
-        {items.map(item => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-      </div>
-    </div>
+    <CollectionsContext.Consumer>
+      {collections => {
+        const collection = collections[match.params.collectionId];
+        const { title, items } = collection;
+        return (
+          <div className="collection-page">
+            <h1 className="title">{title}</h1>
+            <div className="items">
+              {items.map(item => (
+                <CollectionItem key={item.id} item={item} />
+              ))}
+            </div>
+          </div>
+        );
+      }}
+    </CollectionsContext.Consumer>
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
--- src/redux/shop/shop.rducer.js
+++ src/redux/shop/shop.rducer.js
@@ -1,4 +1,4 @@
-import SHOP_DATA from '../../pages/shop/shop.data';
+import SHOP_DATA from '../../contexts/collections/shop.data';
 
 const INITIAL_STATE = {
   collections: SHOP_DATA
