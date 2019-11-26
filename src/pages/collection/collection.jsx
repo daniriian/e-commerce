84418ae246b69301bef353753ef9c980daf396d8
DRT <dryan251@gmail.com>
2019-11-26 15:33:45 +0200

    refactor collection component tio use useContext hook

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useContext } from 'react';
 
 import CollectionItem from '../../components/collection-item/collection-item';
 
@@ -8,23 +8,18 @@ import CollectionsContext from '../../contexts/collections/collections.context';
 import './collection.scss';
 
 const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
   return (
-    <CollectionsContext.Consumer>
-      {collections => {
-        const collection = collections[match.params.collectionId];
-        const { title, items } = collection;
-        return (
-          <div className="collection-page">
-            <h1 className="title">{title}</h1>
-            <div className="items">
-              {items.map(item => (
-                <CollectionItem key={item.id} item={item} />
-              ))}
-            </div>
-          </div>
-        );
-      }}
-    </CollectionsContext.Consumer>
+    <div className="collection-page">
+      <h1 className="title">{title}</h1>
+      <div className="items">
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
   );
 };
 
