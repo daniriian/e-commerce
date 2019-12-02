43c79ae2cd202e1e72de9e5ea49b8e7ed290f23f
Daniel RT <daniriian@gmail.com>
2019-12-02 23:11:18 +0200

    made app mobile ready

--- client/src/components/menu-item/menu-item.jsx
+++ client/src/components/menu-item/menu-item.jsx
@@ -1,21 +1,15 @@
 import React from 'react';
-import './menu-item.scss';
 import { withRouter } from 'react-router-dom';
+import './menu-item.scss';
 
 const MenuItem = props => {
   const { title, imageUrl, size, history, linkUrl, match } = props;
   return (
-    <div
-      className={`${size} menu-item`}
-      onClick={() => history.push(`${match.url}${linkUrl}`)}
-    >
-      <div
-        className='background-image'
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <div className='content'>
-        <h1 className='title'>{title}</h1>
-        <span className='subtitle'>SHOP NOW</span>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+      <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
   );
